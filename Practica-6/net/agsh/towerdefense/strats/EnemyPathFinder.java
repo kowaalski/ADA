74a3ee0cd027b5d1e62081f71d9dbb0bfe6cdca2
Diego García Muñoz <diegomayo.9@hotmail.es>
2023-12-22 09:35:38 +0100

    feat(pr-6): add solution, score 25

--- Practica-6/net/agsh/towerdefense/strats/EnemyPathFinder.java
+++ Practica-6/net/agsh/towerdefense/strats/EnemyPathFinder.java
@@ -2,15 +2,16 @@ package net.agsh.towerdefense.strats;
 
 import net.agsh.towerdefense.*;
 
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.*;
 
 public class EnemyPathFinder {
 
     private static float getWalkableNodeValue(MapNode n, MapNode destination) {
         ArrayList<Tower> towers = Game.getInstance().getMap().getTowers();
         int inRangeTowers = 0;
-        for(Tower t : towers) {
-            if(t.getPosition().distance(n.getPosition()) < 10) {
+        for (Tower t : towers) {
+            if (t.getPosition().distance(n.getPosition()) < 10) {
                 inRangeTowers++;
             }
         }
@@ -21,42 +22,69 @@ public class EnemyPathFinder {
         return 0.5f * distanceFactor + 0.5f * inRangeTowers;
     }
 
-    public static ArrayList<MapNode> findBestPath(MapNode position, ArrayList<MapNode> walkableNodes,
-                                                  ArrayList<MapNode> endingPoints) {
-        // This is just a (bad) example. Replace ALL of this with your own code.
-        // This is a greedy algorithm that (badly) chooses the next node to go to as the closest one to the destination.
+    public static ArrayList<MapNode> findBestPath(MapNode start, ArrayList<MapNode> walkableNodes,
+            ArrayList<MapNode> endingPoints) {
+        Map<MapNode, Integer> fValues = new HashMap<>();
+        PriorityQueue<MapNode> openSet = new PriorityQueue<>(Comparator.comparingInt(fValues::get));
+        Map<MapNode, MapNode> cameFrom = new HashMap<>();
+        Map<MapNode, Integer> gScore = new HashMap<>();
+        Map<MapNode, Integer> fScore = new HashMap<>();
 
-        Game g = Game.getInstance();
-        Map m = g.getMap();
-
-        MapNode origin = position;
-        MapNode destination = endingPoints.get(0);
-
-        // select index 1 in "node values" dropdown in the GUI to see the values of the nodes
-        for(MapNode n : walkableNodes) {
-            n.setValue(1, getWalkableNodeValue(n, destination));
+        for (MapNode node : walkableNodes) {
+            gScore.put(node, Integer.MAX_VALUE);
+            fScore.put(node, Integer.MAX_VALUE);
         }
 
-        ArrayList<MapNode> path = new ArrayList<>();
-        MapNode current = origin;
-        while(current != destination) {
-            MapNode best = null;
-            for(MapNode n : current.getNeighbors()) {
-                if(walkableNodes.contains(n) && !path.contains(n)) {
-                    if(best == null || getWalkableNodeValue(n, destination) < getWalkableNodeValue(best, destination)) {
-                        best = n;
+        gScore.put(start, 0);
+        fScore.put(start, heuristicCostEstimate(start, endingPoints.get(0)));
+        fValues.put(start, fScore.get(start));
+        openSet.add(start);
+
+        while (!openSet.isEmpty()) {
+            MapNode current = openSet.poll();
+
+            if (endingPoints.contains(current)) {
+                return reconstructPath(cameFrom, current);
+            }
+
+            for (MapNode neighbor : current.getNeighbors()) {
+                if (!walkableNodes.contains(neighbor))
+                    continue;
+
+                int tentativeGScore = gScore.get(current) + distBetween(current, neighbor);
+
+                if (tentativeGScore < gScore.get(neighbor)) {
+                    cameFrom.put(neighbor, current);
+                    gScore.put(neighbor, tentativeGScore);
+                    fScore.put(neighbor, gScore.get(neighbor) + heuristicCostEstimate(neighbor, endingPoints.get(0)));
+                    fValues.put(neighbor, fScore.get(neighbor));
+                    if (!openSet.contains(neighbor)) {
+                        openSet.add(neighbor);
                     }
                 }
             }
-
-            if(best != null) { // to avoid infinite loops
-                path.add(best);
-                current = best;
-            } else {
-                return path;
-            }
         }
 
-        return path;
+        return new ArrayList<>(); // return an empty path if there is no path
+    }
+
+    private static int distBetween(MapNode a, MapNode b) {
+        // Use the distance method from your Point2D class
+        return (int) a.getPosition().distance(b.getPosition());
+    }
+
+    private static int heuristicCostEstimate(MapNode a, MapNode b) {
+        // Use the distance method from your Point2D class
+        return (int) a.getPosition().distance(b.getPosition());
+    }
+
+    private static ArrayList<MapNode> reconstructPath(Map<MapNode, MapNode> cameFrom, MapNode current) {
+        ArrayList<MapNode> totalPath = new ArrayList<>();
+        totalPath.add(current);
+        while (cameFrom.containsKey(current)) {
+            current = cameFrom.get(current);
+            totalPath.add(0, current);
+        }
+        return totalPath;
     }
 }
